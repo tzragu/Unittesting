fcfac88a8ff86e21f9f3c34622ff261b7cc23df3
darwyncook <cook@alfred.edu>
2017-05-03 10:57:21 -0400

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,3 +1,7 @@
+import hashlib
+import re
+
+
 class InvalidEmail(Exception):
     pass
 
@@ -9,12 +13,12 @@ class InvalidSocial(Exception):
 class InvalidPassword(Exception):
     pass
 
-
 class Email:
 
     def __init__(self, email):
-        if "@" in email:
-            self.email = email
+        search_Obj = re.search(r'[a-zA-Z]([a-zA-Z0-9]*)@([a-zA-Z0-9]*)\.edu|biz|com|gov', email, flags=0)
+        if search_Obj is not None:
+            self.email = search_Obj.group()
         else:
             raise InvalidEmail
 
@@ -25,25 +29,35 @@ class Email:
 class SS:
 
     def __init__(self, social):
-        if "-" in social:
+        search_Obj = re.search(r'[0-9]{3}-[0-9]{2}-[0-9]{4}', social, flags=0)
+        if search_Obj is not None:
             self.social = social
         else:
             raise InvalidSocial
 
+    def __str__(self):
+        return self.social
+
 
 class Hash:
-    x = 0
-
     def __init__(self, passwd):
-        if len(passwd) > 1:
+        if len(passwd) > 7:
             self.hash = self.generate_hash(passwd)
         else:
             raise InvalidPassword
 
-    def __eq__(self, passwd):
-        return self.hash == self.generate_hash(passwd)
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.hash == self.generate_hash(other)
+        elif isinstance(other, Hash):
+            return self.hash == other.hash
+        else:
+            return NotImplemented
 
     @staticmethod
     def generate_hash(passwd):
-        Hash.x += 1
-        return Hash.x
+        """ This is NOT secure, a random salt should be added"""
+        return hashlib.sha512(passwd.encode("utf-8")).hexdigest()
+
+    def __str__(self):
+        return self.hash
